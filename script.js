3f87a6d429cf35614871f7e397bad02a7f16a2e9
CELIO LAZARO PEREIRA <celio.lpereira@senacsp.edu.br>
2024-09-24 15:16:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Solicitar o nome do usuário
+let nome = prompt("Insira o seu nome");
+
+// Insira a idade do usuário
+let idade = parseInt(prompt("Insira sua idade"));
+
+// Calcular
+let anosparacem = 100 - idade
+
+// Apresentar para o usuário um alerta
+alert (`${nome}, faltam ${anosparacem} anos para você completar 100 anos`);
